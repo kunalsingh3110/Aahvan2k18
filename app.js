490fdaff8f0a366dd78e0d62fd693e56654225c8
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-12-09 19:01:57 +0530

    oops forgot to delete them

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var express = require("express");
-var hbs = require('hbs'); // view engine for handlebars
 var app = express();
 
-mongoose.connect("mongodb://localhost/aahvan2k18", {useMongoClient:true});
+mongoose.connect("mongodb://localhost/aahvan2k18");
 
 
-//FOR FRONT END, USING HANDLEBARS INSTEAD
-// app.set("view engine","ejs");
 
-hbs.registerPartials(__dirname + "/views/partials");
-app.set("view engine", "hbs");
-
-app.use(express.static(__dirname+'/public'));
+app.set("view engine","ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
@@ -32,11 +27,6 @@ app.get("/live",function(req,res){
 	res.render("live");
 });
 
-
-//FOR DEVELOPMENT PORT IS 3000
-//FOR PRODUCTION PORT IS AUTOMATICALLY DECIDED
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT,() => {
-	console.log('SERVER HAS STARTED ON PORT:',PORT);
+app.listen(3000,function(){
+	console.log("Server started!!");
 });
