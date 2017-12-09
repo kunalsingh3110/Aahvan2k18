ddba37e1d90515a8077d41718a034965d231a62b
Gauravkumar10094559 <karthikkumar10094559@gmail.com>
2017-12-09 17:07:43 +0530

    change in template

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var express = require("express");
+var hbs = require('hbs'); // view engine for handlebars
 var app = express();
 
-mongoose.connect("mongodb://localhost/aahvan2k18");
+mongoose.connect("mongodb://localhost/aahvan2k18", {useMongoClient:true});
 
 
+//FOR FRONT END, USING HANDLEBARS INSTEAD
+// app.set("view engine","ejs");
 
-app.set("view engine","ejs");
-app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
@@ -27,6 +32,11 @@ app.get("/live",function(req,res){
 	res.render("live");
 });
 
-app.listen(3000,function(){
-	console.log("Server started!!");
+
+//FOR DEVELOPMENT PORT IS 3000
+//FOR PRODUCTION PORT IS AUTOMATICALLY DECIDED
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+	console.log('SERVER HAS STARTED ON PORT:',PORT);
 });
