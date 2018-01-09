0586cd72a1eeff1d1648b2fd37bab6f96bc586cc
kinglesnar <singh.kunal112@gmail.com>
2018-01-09 07:06:34 +0530

    managing merge

--- public/index.js
+++ public/index.js
@@ -26,17 +26,6 @@ $(document).ready(function(){
 
 
 $('#Events').on("click",function() {
-<<<<<<< HEAD
-  jQuery("html, body").animate({scrollTop:  $("#particles-js").offset().top }, 2000);   
-});
-
-$('#Theme').on("click",function() {
-  jQuery("html, body").animate({scrollTop:  $(".theme").offset().top }, 2000);   
-});
-
-$('#Lookback').on("click",function() {
-  jQuery("html, body").animate({scrollTop:  $(".lookback").offset().top }, 2000);   
-=======
   jQuery("#home").animate({scrollTop:  $(".events").offset().top }, 1000);   
 });
 
@@ -50,12 +39,9 @@ $('#CA').on("click",function() {
 
 $('#Lookback').on("click",function() {
   jQuery("#home").animate({scrollTop:  $(".lookback-text").offset().top }, 2000);   
->>>>>>> 7938e686afd1d9549804afaa301819b72aa39c70
 });
 
-$('#Live').on("click",function() {
-  jQuery("html, body").animate({scrollTop:  $(".live").offset().top }, 2000);   
-});
+
 
 
   // window.onscroll = () => {
@@ -83,17 +69,17 @@ $('#Live').on("click",function() {
 
 
   $('#name_logout').hover(function(){
-		text = $(this).text();
-		$(this).text("Logout");
-	},function(){
-		$(this).text(text);
-	});
+    text = $(this).text();
+    $(this).text("Logout");
+  },function(){
+    $(this).text(text);
+  });
 
 
-	$('.col').hover(function(){
-		$('.col').not(this).toggleClass('scale-down');
-		$(this).toggleClass('scale-up');
-	});
+  $('.col').hover(function(){
+    $('.col').not(this).toggleClass('scale-down');
+    $(this).toggleClass('scale-up');
+  });
 
 $('#number').on('input',function(){
   $('.team-players').empty();
@@ -197,118 +183,6 @@ $('#sort').on('change',function(){
 
 });
 
-<<<<<<< HEAD
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 150,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 10
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-=======
 // particlesJS("particles-js", {
 //   "particles": {
 //     "number": {
@@ -419,124 +293,9 @@ particlesJS("particles-js", {
 //   },
 //   "retina_detect": true
 // });
->>>>>>> 7938e686afd1d9549804afaa301819b72aa39c70
-
-particlesJS("particles-js-two",{
-  "particles": {
-    "number": {
-      "value": 6,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#1b1e34"
-    },
-    "shape": {
-      "type": "polygon",
-      "stroke": {
-        "width": 0,
-        "color": "#000"
-      },
-      "polygon": {
-        "nb_sides": 6
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 160,
-      "random": false,
-      "anim": {
-        "enable": true,
-        "speed": 10,
-        "size_min": 40,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 200,
-      "color": "#ffffff",
-      "opacity": 1,
-      "width": 2
-    },
-    "move": {
-      "enable": true,
-      "speed": 8,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-
-
 
 AOS.init({
   duration: 600,
 });
 
 });
-
